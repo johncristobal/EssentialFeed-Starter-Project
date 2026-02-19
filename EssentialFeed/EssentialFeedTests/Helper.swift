b8ecdec68ef7f9df6ec69513ab1d71edbe8a0dc5
johnsurexs <john.cristobal@surexs.com>
2026-02-18 21:39:13 -0600

    add comment demo for test CI

--- EssentialFeed/EssentialFeedTests/Helper.swift
+++ EssentialFeed/EssentialFeedTests/Helper.swift
@@ -7,6 +7,7 @@
 
 import XCTest
 
+// trackForMemoryLeaks Helper function generic
 extension XCTestCase {
     func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #file, line: UInt = #line) {
         addTeardownBlock { [weak instance] in
